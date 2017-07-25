89919900c91e69458b11cf62a503e934d0f6a633
uipo78 <brandencarrier@gmail.com>
2017-07-25 17:19:37 -0500

    Add header comment

---
+++ genre_clf_nn/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+'''
+    Branden Carrier
+    07/25/2017
+
+    This purpose of this script is to build a neural network that classifies a
+    song's genre based on its audio.
+'''
+
+import keras
+import utils
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
