7c96d09fff40dc93cadaff899f7fd4b82a2b90be
uipo78 <brandencarrier@gmail.com>
2017-07-25 20:54:07 -0500

    Add format_audio and make_model_input_generator

--- genre_clf_nn/main.py
+++ genre_clf_nn/main.py
@@ -9,6 +9,7 @@
 '''
 
 import keras
+import librosa
 import utils
 
 from keras import (
@@ -16,6 +17,28 @@ from keras import (
     GRU,
     Sequential
 )
+
+
+def format_audio(img_path, sr = 44100, n_fft = 2048, hop_length = 512):
+    audio_time_series, _ = librosa.load(img_path,
+                                        sr = sr)
+    S = librosa.feature.melspectrogram(y = audio_time_series,
+                                       sr = sr,
+                                       n_fft = n_fft,
+                                       hop_length = hop_length)
+
+    return S.T
+
+
+def make_model_input_generator(files, batch_size = 128):
+    to_stack = []
+    for filepath in files:
+        formatted_audio = format_audio(filepath)
+        to_stack.append(formatted_audio)
+
+        if len(to_stack) == batch_size:
+            yield np.stack(to_stack, axis = 0)
+
 
 def main():
 
