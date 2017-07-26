1939fe1ad8aa01a40ac550517e0f82907c2260ac
uipo78 <brandencarrier@gmail.com>
2017-07-25 20:53:15 -0500

    Add content to main

--- genre_clf_nn/main.py
+++ genre_clf_nn/main.py
@@ -13,7 +13,31 @@ import utils
 
 
 def main():
-    pass
+
+    # Instantiate model
+    model = Sequential()
+
+    # Add first layer
+    model.add(GRU(
+
+    ))
+
+    # Add hidden layers
+
+    # Add final layer
+    model.add(Dense(
+        output_dim = SOMETHING,
+        activation = SOMETHING
+    ))
+
+    # Compile model
+    model.compile(
+        optimizer = 'adam',
+        loss = 'categorical_crossentropy',
+        metrics = ['accuracy']
+    )
+
+    model.fit_generator()
 
 if __name__ == '__main__':
     main()
